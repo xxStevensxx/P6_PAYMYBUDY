c4747267e19872659d96dd60df5469a5721fa52c
xxStevensxx <29457062+xxStevensxx@users.noreply.github.com>
2021-07-23 17:54:15 +0200

    auhentication inMemory ok

--- src/main/java/com/pay/my/budy/config/SecurityConfiguration.java
+++ src/main/java/com/pay/my/budy/config/SecurityConfiguration.java
@@ -2,26 +2,29 @@ package com.pay.my.budy.config;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
-	@Autowired
-	DataSource dataSource;
+//	@Autowired
+//	DataSource dataSource;
 
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
-		auth.jdbcAuthentication()
-				.dataSource(dataSource);
+		auth.inMemoryAuthentication()
+			.withUser("test@test")
+			.password(passwordEncoder().encode("test"))
+			.roles("USER");	
 				
 	}
 	
@@ -30,13 +33,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		http.authorizeRequests()
-			.antMatchers(" /transfert "," /profil "," /contact ").hasAnyRole("USER", "ADMIN")
-			.antMatchers("/home","/").permitAll()
+			.antMatchers("/", "/home", "/transfert", "/contact", "/logout").authenticated()
 			.and()
 			.formLogin()
 			.loginPage("/signin")
-			.loginProcessingUrl("/signin")
-			.defaultSuccessUrl("/home");
+			.permitAll();
 		
 	}
 	
@@ -44,7 +45,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(WebSecurity web) {
 		
-		web.ignoring().antMatchers("/resources/*");
+		web.ignoring().antMatchers("/resources/**");
 		
 	}
 	
