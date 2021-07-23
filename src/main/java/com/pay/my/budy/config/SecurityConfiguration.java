ca3f7ca54111e633abbc94f9d189b7f93afe04a3
xxStevensxx <29457062+xxStevensxx@users.noreply.github.com>
2021-07-23 17:54:15 +0200

    auhentication inMemory ok

--- src/main/java/com/pay/my/budy/config/SecurityConfiguration.java
+++ src/main/java/com/pay/my/budy/config/SecurityConfiguration.java
@@ -3,7 +3,6 @@ package com.pay.my.budy.config;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -19,15 +18,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	DataSource dataSource;
-	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
-		auth.jdbcAuthentication()
-				.dataSource(dataSource);
-				
-	}
-	
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
--- src/main/java/com/pay/my/budy/controller/HomeController.java
+++ src/main/java/com/pay/my/budy/controller/HomeController.java
@@ -12,4 +12,6 @@ public class HomeController {
 		
 		return "/layouts/index";
 	}
+
 }
+
