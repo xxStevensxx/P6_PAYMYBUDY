3890aa29fc2670b089d681ec80e1628d5692f1da
xxStevensxx <29457062+xxStevensxx@users.noreply.github.com>
2021-08-13 11:17:51 +0100

    ajotu de l'anotation @Controller sur les controlleurs

--- src/main/java/com/pay/my/budy/controller/ProfilController.java
+++ src/main/java/com/pay/my/budy/controller/ProfilController.java
@@ -1,7 +1,9 @@
 package com.pay.my.budy.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class ProfilController {
 	
 	@GetMapping(value = "/profil")
