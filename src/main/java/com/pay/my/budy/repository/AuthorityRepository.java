399b2a88dc415833d7335a776fbabeea47f1280a
Stevens <29457062+xxStevensxx@users.noreply.github.com>
2021-08-26 18:55:26 +0200

    Creation de l'interface Aut..repository qui etend jpaRepo...

---
+++ src/main/java/com/pay/my/budy/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.pay.my.budy.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pay.my.budy.model.Authority;
+
+public interface AuthorityRepository extends JpaRepository<Authority, Integer> {
+
+}
