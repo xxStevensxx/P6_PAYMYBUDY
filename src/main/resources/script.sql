72f077253c1ecbc8d4dbc9de26a55f76abc9d2ae
Stevens <29457062+xxStevensxx@users.noreply.github.com>
2021-08-27 13:59:23 +0200

    ajout  des tables de session

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -113,6 +113,40 @@ CREATE TABLE IF NOT EXISTS `users` (
 INSERT INTO `users` (`username`, `name`, `firstname`, `birthdate`, `address`, `password`) VALUES
 ('test@test', 'test', 'test-test', NULL, 'rue de test', '$2y$10$jWvX8H.8XzHZkfZdP5pOY.1jXkH2HsLPCMTnINYCTtYL76F0saeHS');
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `SPRING_SESSION`
+--
+
+DROP TABLE IF EXIST `SPRING_SESSION`;
+CREATE TABLE IF NOT EXISTS `SPRING_SESSION` (
+`primary_id` CHAR(36) NOT NULL,
+`session_id` CHAR(36) NOT NULL,
+`creation_time` BIGINT NOT NULL,
+`last_access_time` BIGINT NOT NULL,
+`max_inactive_interval` INT NOT NULL,
+`expiry_time` BIGINT NOT NULL,
+`principal_name` VARCHAR(100),
+CONSTRAINT spring_session_pk PRIMARY KEY (primary_id)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `SPRING_SESSION_ATTRIBUTES`
+--
+DROP TABLE IF EXIST `SPRING_SESSION_ATTRIBUTES`;
+CREATE TABLE IF NOT EXISTS SPRING_SESSION_ATTRIBUTES (
+`session_primary_id` CHAR(36) NOT NULL,
+`attribute_name` VARCHAR(200) NOT NULL,
+`attribute_bytes` BLOB NOT NULL,
+CONSTRAINT spring_session_attributes_pk PRIMARY KEY (session_primary_id, attribute_name),
+CONSTRAINT spring_session_attributes_fk FOREIGN KEY (session_primary_id) REFERENCES SPRING_SESSION(primary_id) ON DELETE CASCADE
+);
+
+-- --------------------------------------------------------
+
 --
 -- Contraintes pour les tables déchargées
 --
