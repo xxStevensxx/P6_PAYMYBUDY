dab310c641de7b2d5be2ec2126340472a0aaed9b
Stevens <29457062+xxStevensxx@users.noreply.github.com>
2021-09-01 19:09:32 +0200

    Modification des attribut sql dans les propeerties ajout de la gestion de session dans le homneController, ajustement des fragments dans le layouts profil et modification des attributys de session dans le frqagment session

--- src/main/java/com/pay/my/budy/controller/HomeController.java
+++ src/main/java/com/pay/my/budy/controller/HomeController.java
@@ -1,16 +1,35 @@
 package com.pay.my.budy.controller;
 
+
+import java.security.Principal;
+import java.util.Collection;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.session.FindByIndexNameSessionRepository;
+import org.springframework.session.Session;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+
 
 @Controller
 public class HomeController {
 	
+	@Autowired
+	FindByIndexNameSessionRepository<? extends Session> sessions;
 	
 	@GetMapping(value = {"/", "/home"})
-	public String homeController() {
+	public String homeController(Principal principal, Model model, HttpSession session) {
+				
+		model.addAttribute("session_name", principal.getName());		
+		session.setAttribute("session_name", model.getAttribute("session_name"));
 		
-		return "/layouts/index";
+		System.err.println(session.getAttribute("session"));
+		
+			return "/layouts/index";
 	}
 }
 
