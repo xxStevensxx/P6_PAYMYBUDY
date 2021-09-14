453aed7109da3a29c34db9c3c4bf10e9bba0fb18
Stevens <29457062+xxStevensxx@users.noreply.github.com>
2021-09-14 19:42:02 +0200

    Ajout de l'annotation @services

--- src/main/java/com/pay/my/budy/service/ProfilServices.java
+++ src/main/java/com/pay/my/budy/service/ProfilServices.java
@@ -1,12 +1,15 @@
 package com.pay.my.budy.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.pay.my.budy.dto.Mapper;
 import com.pay.my.budy.dto.UserDTO;
 import com.pay.my.budy.model.User;
 import com.pay.my.budy.repository.UserRepository;
 
+
+@Service
 public class ProfilServices {
 	
 	@Autowired
