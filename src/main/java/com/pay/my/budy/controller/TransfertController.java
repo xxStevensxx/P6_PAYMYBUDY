f3995c3e63fef15683b8f9b6eaa6fe77bd1dc884
xxStevensxx <29457062+xxStevensxx@users.noreply.github.com>
2021-09-15 12:33:36 +0200

    Mappage des donnée a la vue

--- src/main/java/com/pay/my/budy/controller/TransfertController.java
+++ src/main/java/com/pay/my/budy/controller/TransfertController.java
@@ -1,13 +1,27 @@
 package com.pay.my.budy.controller;
 
+import java.security.Principal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.pay.my.budy.dto.UserDTO;
+import com.pay.my.budy.service.RelationShipServices;
 
 @Controller
 public class TransfertController {
 	
+	@Autowired
+	RelationShipServices friendShipServices;
+	
 	@GetMapping(value = "/transfert")
-	public String transfertController() {
+	public String transfertController(Principal principal, Model model) {
+		
+		List<UserDTO> friends = friendShipServices.getConnections(principal.getName());
+		model.addAttribute("friends", friends);
 		
 		return "/layouts/transfert";
 	}
