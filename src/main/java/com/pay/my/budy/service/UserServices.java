a36c28349b71801561e01655c9eb2edfe60f8cb3
Stevens <29457062+xxStevensxx@users.noreply.github.com>
2021-09-19 23:03:45 +0200

    Fix coquilles vides

--- src/main/java/com/pay/my/budy/service/UserServices.java
+++ src/main/java/com/pay/my/budy/service/UserServices.java
@@ -5,6 +5,7 @@ package com.pay.my.budy.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
 
 import com.pay.my.budy.config.SecurityConfiguration;
 import com.pay.my.budy.constant.Role;
@@ -50,25 +51,36 @@ public class UserServices {
 	 */
 	public void signup(UserSignupDTO userSignupDTO) {	
 		
+		
 		User user = mapper.toUser(userSignupDTO);
-	
-		user.setPassword(security.passwordEncoder().encode(user.getPassword()));
-		userRepository.save(user);
-		
 		User user_info = userRepository.findByusername(user.getUsername());
+
 		
-		Bankaccount bank = new Bankaccount();
-		bank.setIdUser(user_info.getId());
-		bank.setMoneyAvailable(0.0);
-		
-		bankRepository.save(bank);	
-		
-		Authority authority = new Authority();
-		authority.setUser_id(user_info.getId());
-		authority.setUsername(user.getUsername());
-		authority.setAuthority(Role.USER);
-		
-		authRepository.save(authority);
+		if (user_info == null) {
+			
+			user.setPassword(security.passwordEncoder().encode(user.getPassword()));
+			userRepository.save(user);
+			
+			User userToPersist = userRepository.findByusername(user.getUsername());
+
+			
+			Bankaccount bank = new Bankaccount();
+			bank.setIdUser(userToPersist.getId());
+			bank.setMoneyAvailable(0.0);
+			
+			bankRepository.save(bank);	
+			
+			Authority authority = new Authority();
+			authority.setUser_id(userToPersist.getId());
+			authority.setUsername(user.getUsername());
+			authority.setAuthority(Role.USER);
+			
+			authRepository.save(authority);
+			
+			
+		}
+	
+
 		
 	}
 	
